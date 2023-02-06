555bc4a1dc46b571e58a8c7ead61d0d5a182ef4e
Sergey <114340735+rebelly@users.noreply.github.com>
2023-02-06 12:35:24 +0300

    Update all.cs

--- all.cs
+++ all.cs
@@ -1,8 +1,8 @@
-using System; // все работает только с положительными числами
+using System; 
 
 class Program
 {
-    static void output_mass(int[,] mass)
+    static void output_mass(string[,] mass)
     {
         for (int i = 0; i < mass.GetLength(0); i++)
 
@@ -12,32 +12,50 @@ class Program
                 Console.Write($"{mass[i, j]}\t");
             }
             Console.WriteLine();
-            
+
         }
     }
-    static void snake(ref int[,] mass)
+    static void snake(ref string[,] mass)
     {
         for (int i = 0; i < mass.GetLength(1); i++)
 
         {
             for (int j = 0; j < mass.GetLength(1); j++)
             {
-                if (i % 2 != 0) mass[i, j] =  mass.GetLength(1)*(i+1)-j;
-                else mass[i, j] = j + mass.GetLength(1) * i+1;
+                if (i % 2 != 0) mass[i, j] = Convert.ToString( mass.GetLength(1) * (i + 1) - j);
+                else mass[i, j] = Convert.ToString(j + mass.GetLength(1) * i + 1);
             }
 
         }
 
     }
-    static int max_elem_from_zone(int [, ] mass)
+    static void snow(ref string[,] mass)
     {
-        int res = 0;
-        for (int i =0; i < mass.GetLength(0); i++)
+        for (int i  = 0; i < mass.GetLength(0); i++)
         {
-            for (int j = mass.GetLength(1)-i; j < mass.GetLength(1); j++)
+            for (int j = 0; j < mass.GetLength(1); j++)
             {
-                if (i < j && (i + j) <= mass.GetLength(0)-2) Console.WriteLine(mass[i, j]);
-                else if (i > j&&(i + j) >= mass.GetLength(0) - 2) Console.WriteLine(mass[i, j]);
+                if (i == (int)(mass.GetLength(0) / 2) || j == (int)(mass.GetLength(0) / 2) || i == j || i + j == mass.GetLength(0)-1)
+                {
+                    mass[i, j] = "*";
+
+                }
+                else
+                {
+                    mass[i, j] = " ";
+                }
+            }
+        }
+    }
+    static int max_elem_from_zone(int[,] mass)
+    {
+        int res = int.MinValue;
+        for (int i = 0; i < mass.GetLength(0); i++)
+        {
+            for (int j = 0; j < mass.GetLength(1); j++)
+            {
+                if (i < j && i + j < mass.GetLength(0) - 1) { if (mass[i, j] > res) res = mass[i, j]; }
+                else if (i > j && i + j > mass.GetLength(0) - 1) { if (mass[i, j] > res) res = mass[i, j]; }
             }
         }
         return res;
@@ -45,10 +63,16 @@ class Program
     }
     public static void Main()
     {
-        int[,] mass = new int[4, 4];
+        string[,] mass = new string[4, 4];
         snake(ref mass);
-        int[,] mass1 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } , { 13, 14, 15, 16 } };
+        output_mass(mass);
+        Console.WriteLine("Введите длину снежинки");
+        int n = int.Parse(Console.ReadLine());
+        string[,] mass1 = new string[n, n];
+        snow(ref mass1);
         output_mass(mass1);
-        max_elem_from_zone(mass1);
+        int[,] mass2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        Console.WriteLine(max_elem_from_zone(mass2));
+        
     }
 }
